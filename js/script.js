c199fff805cb01ef8e768ea32cf83d0288b43669
gusttavares97 <gustavotavares018@gmail.com>
2023-03-01 10:47:16 -0300

    horario-funcionamento

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import fetchBitcoin from "./fetchbitcoin.js";
 import toggleMenu from "./menu.js";
+import initfuncionamento from "./funcionamento.js";
 
 fetchBitcoin();
-toggleMenu()
+toggleMenu();
+initfuncionamento()
